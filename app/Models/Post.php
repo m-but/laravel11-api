afacf78347c38c38bce31b8166cf7382edd186e1
m-but <budiman1347@gmail.com>
2024-10-18 15:58:48 +0700

    create model and migration Post, add accessor image in model Post

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Post extends Model
+{
+    protected $fillable = ["image", "title", "content"];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url("/storage/posts/" . $image),
+        );
+    }
+}
